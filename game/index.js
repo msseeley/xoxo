58842ee1876adcc9e85082fc70256bad442fde18
Molly Seeley <mseeley89@gmail.com>
2018-05-16 14:42:43 -0400

    working on the who won

--- game/index.js
+++ game/index.js
@@ -1,6 +1,26 @@
-import {Map} from 'immutable'
+import { Map } from "immutable";
 
-export default function reducer(state, action) {
+const MOVE = "MOVE";
+
+export const move = (turn, position) => ({ type: MOVE, turn, position });
+
+const initialState = {
+  turn: "X",
+  board: Map()
+};
+
+export default function reducer(state = initialState, action) {
+  let newState = Object.assign({}, state);
+  // console.log(newState);
+  switch (action.type) {
+    case MOVE:
+      const newTurn = action.turn === "X" ? "O" : "X";
+      newState.board = newState.board.setIn(action.position, action.turn);
+      newState.turn = newTurn;
+      console.log(newState);
+      return newState;
+    default:
+      return state;
+  }
   // TODO
-  return state
 }
