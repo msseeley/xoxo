516fbd0e74af3584f98262c5d64711d02288afd2
Sharon Yun <sharonyun11@gmail.com>
2018-05-16 16:34:18 -0400

    Streaks completed

--- game/index.js
+++ game/index.js
@@ -1,26 +1,45 @@
-import { Map } from "immutable";
+import { Map } from 'immutable';
 
-const MOVE = "MOVE";
+const MOVE = 'MOVE';
+const TURN = 'TURN';
+const POSITION = 'POSITION';
 
+// export const turn = turn => ({ type: MOVE, turn });
+// export const position = position => ({ type: MOVE, position });
 export const move = (turn, position) => ({ type: MOVE, turn, position });
 
 const initialState = {
-  turn: "X",
+  turn: 'X',
   board: Map()
 };
 
-export default function reducer(state = initialState, action) {
-  let newState = Object.assign({}, state);
-  // console.log(newState);
-  switch (action.type) {
-    case MOVE:
-      const newTurn = action.turn === "X" ? "O" : "X";
-      newState.board = newState.board.setIn(action.position, action.turn);
-      newState.turn = newTurn;
-      console.log(newState);
-      return newState;
-    default:
-      return state;
-  }
-  // TODO
+export function turnReducer(turn = 'X', action) {
+  if (action.type === MOVE) return turn === 'X' ? 'O' : 'X';
+  return turn;
 }
+
+export function boardReducer(board = Map(), action) {
+  if (action.type === MOVE) return board.setIn(action.coord, action.player);
+  return board;
+}
+
+export default function reducer(state = initialState, action) {
+  return {
+    board: boardReducer(state.board, action),
+    turn: turnReducer(state.turn, action)
+  };
+}
+
+// export default function reducer(state = initialState, action) {
+//   let newState = Object.assign({}, state);
+//   switch (action.type) {
+//     case MOVE: {
+//       const newTurn = action.turn === 'X' ? 'O' : 'X';
+//       newState.board = newState.board.setIn(action.position, action.turn);
+//       newState.turn = newTurn;
+//       return newState;
+//     }
+//     default:
+//       return state;
+//   }
+// }
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import inquirer from "inquirer";
+import inquirer from 'inquirer';
 
-import gameReducer, { move } from "./game";
-import { createStore } from "redux";
+import gameReducer, { move } from './game';
+import { createStore } from 'redux';
 
 const printBoard = () => {
   const { board } = game.getState();
   for (let r = 0; r != 3; ++r) {
     for (let c = 0; c != 3; ++c) {
-      process.stdout.write(board.getIn([r, c], "_"));
+      process.stdout.write(board.getIn([r, c], '_'));
     }
-    process.stdout.write("\n");
+    process.stdout.write('\n');
   }
 };
 
@@ -18,8 +18,8 @@ const getInput = player => async () => {
   if (turn !== player) return;
   const ans = await inquirer.prompt([
     {
-      type: "input",
-      name: "coord",
+      type: 'input',
+      name: 'coord',
       message: `${turn}'s move (row,col):`
     }
   ]);
@@ -30,13 +30,65 @@ const getInput = player => async () => {
 // Create the store
 const game = createStore(gameReducer);
 
+const winner = () => {
+  const { board } = game.getState();
+
+  // Building an array of coordinates
+  let arr = [];
+  for (let i = 0; i < 3; ++i) {
+    let row = [];
+    for (let k = 0; k < 3; ++k) {
+      row.push([i, k]);
+    }
+    arr.push(row);
+  }
+
+  const row1 = arr[0];
+  const row2 = arr[1];
+  const row3 = arr[2];
+
+  const top = streak(board, row1);
+  const middle = streak(board, row2);
+  const bottom = streak(board, row3);
+  const diag1 = streak(board, [[0, 0], [1, 1], [2, 2]]);
+  const diag2 = streak(board, [[2, 0], [1, 1], [0, 2]]);
+
+  let result;
+  if (top) {
+    result = top;
+  } else if (middle) {
+    result = middle;
+  } else if (bottom) {
+    result = bottom;
+  } else if (diag1) {
+    result = diag1;
+  } else if (diag2) {
+    result = diag2;
+  } else {
+    return 'DRAW!';
+  }
+
+  console.log('The winner is ', result);
+};
+
+const streak = (board, coords) => {
+  if (
+    board.getIn(coords[0]) === board.getIn(coords[1]) &&
+    board.getIn(coords[0]) === board.getIn(coords[2])
+  ) {
+    return board.getIn(coords[0]);
+  }
+};
+
+game.subscribe(winner);
+
 // Debug: Print the state
 game.subscribe(() => console.log(game.getState()));
 
 game.subscribe(printBoard);
-game.subscribe(getInput("X"));
-game.subscribe(getInput("O"));
+game.subscribe(getInput('X'));
+game.subscribe(getInput('O'));
 
 // We dispatch a dummy START action to call all our
 // subscribers the first time.
-game.dispatch({ type: "START" });
+game.dispatch({ type: 'START' });
